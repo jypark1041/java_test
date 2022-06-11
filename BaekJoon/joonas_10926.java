fc3654573019a96fc05d13d0dbf1c5ac844499c0
허유민 <fodleldjs@naver.com>
2022-06-12 00:22:34 +0900

    ?!!_10926

---
+++ BaekJoon/joonas_10926.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class joonas_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id;
+        id = sc.next().toLowerCase();
+        System.out.println(id + "??!");
+    }
+}
---
+++ Cal_java/calab.java
@@ -0,0 +1,17 @@
+package Cal_java;
+
+import java.util.Scanner;
+
+public class calab {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
